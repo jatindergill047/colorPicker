1719e4c456fe1df1ef061a2f727a4670383828f5
Jatinder Singh <801937@ACDM.DS.SAIT.CA>
2018-10-18 17:01:38 -0600

    Initial commit

---
+++ logic.js
@@ -0,0 +1,26 @@
+$('#subbtn').on('click', function(event) {
+    event.preventDefault();
+    // $('#palettes').html(''); 
+$("#palettes #box").remove();
+
+var colorName = $('#nameOfColor').val();
+var paleteNumber = $('#numOfPalette').val();
+var lum = $('#luminosity').val();
+
+console.log(colorName);
+console.log(paleteNumber);
+console.log(lum);
+
+var color = randomColor({hue: colorName, luminosity: lum, count: paleteNumber});
+
+$.each(color, function(i,clr)
+{
+    var palette = $('<div id="box"><p></p><div>')
+    palette.css({'background-color':clr });
+    $('#palettes').append(palette);
+    console.log(color[i]);
+
+})
+
+});
+
