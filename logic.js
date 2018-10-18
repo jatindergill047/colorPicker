802b83475230b995eafeb4492c5196fda41e3bd0
Jatinder Singh <801937@ACDM.DS.SAIT.CA>
2018-10-18 17:02:43 -0600

    done

--- logic.js
+++ logic.js
@@ -1,26 +1,25 @@
 $('#subbtn').on('click', function(event) {
-    event.preventDefault();
-    // $('#palettes').html(''); 
-$("#palettes #box").remove();
+        event.preventDefault();
+        // $('#palettes').html(''); 
+    $("#palettes #box").remove();
 
-var colorName = $('#nameOfColor').val();
-var paleteNumber = $('#numOfPalette').val();
-var lum = $('#luminosity').val();
+    var colorName = $('#nameOfColor').val();
+    var paleteNumber = $('#numOfPalette').val();
+    var lum = $('#luminosity').val();
 
-console.log(colorName);
-console.log(paleteNumber);
-console.log(lum);
+    console.log(colorName);
+    console.log(paleteNumber);
+    console.log(lum);
 
-var color = randomColor({hue: colorName, luminosity: lum, count: paleteNumber});
+    var color = randomColor({hue: colorName, luminosity: lum, count: paleteNumber});
 
-$.each(color, function(i,clr)
-{
-    var palette = $('<div id="box"><p></p><div>')
-    palette.css({'background-color':clr });
-    $('#palettes').append(palette);
-    console.log(color[i]);
+    $.each(color, function(i,clr)
+    {
+        var palette = $('<div id="box"><p></p><div>')
+        palette.css({'background-color':clr });
+        $('#palettes').append(palette);
+        console.log(color[i]);
 
-})
-
+    })
 });
 
